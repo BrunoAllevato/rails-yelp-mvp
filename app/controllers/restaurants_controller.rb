6751e91d049f76fed0cfb389dd7beddb0adaeafb
Bruno Allevato <brunoallevato@gmail.com>
2022-10-14 19:20:44 +0100

    rails yelp mvp done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -31,7 +31,6 @@ class RestaurantsController < ApplicationController
   # end
 
   def destroy
-    @restaurant = Restaurant.find(params[:id])
     @restaurant.destroy
     redirect_to restaurants_path
   end
